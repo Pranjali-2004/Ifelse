3cb2e628027b9db64186123a47208f2609128157
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-25 21:34:21 +0530

    Add files via upload

---
+++ inputNumbers.java
@@ -0,0 +1,9 @@
+public class inputNumbers {
+    public static void main(String[]args){
+        int a= 3;
+        int b=6;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
