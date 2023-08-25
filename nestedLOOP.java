9ba9ef34e13dbd752cbbe6d9775619efc762c88a
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-25 21:53:32 +0530

    Add files via upload

---
+++ nestedLOOP.java
@@ -0,0 +1,12 @@
+public class nestedLOOP {
+    public static void main(String[]args){
+        //Printing rectangle
+        for(int i=0; i<=2; i++){
+            for(int j=0; j<=8; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
