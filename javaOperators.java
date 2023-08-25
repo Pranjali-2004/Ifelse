84f4435faa35eca80685ae1992631bfa5eb79a96
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-25 21:53:11 +0530

    Add files via upload

---
+++ javaOperators.java
@@ -0,0 +1,31 @@
+public class javaOperators {
+    public static void main(String[]args){
+        int a=98;
+        int b= 45;
+        //ARITHMETIC OPERATIONS
+        //ADD
+        int sum= a+b;
+        System.out.println("THE SUM OF TWO NUMBERS IS= "+sum);
+        //SUBTRACT
+        int sub= a-b;
+        System.out.println("THE SUBTRACTION OF TWO NUMBERS ARE="+sub);
+        //MULTIPLY
+        int mut= a*b;
+        System.out.println("THE MULTIPLICATION OF TWO NUMBERS IS= "+mut);
+        //MODULAS
+        int mod= a%b;
+        System.out.println("THE MODULAS OF TWO NUMBER IS= "+mod);
+        //RELATIONAL OPERATORS
+        //IS EQUAL TO
+        Boolean n= a==b;
+        System.out.println(n);
+        //NOT EQUAL TO
+        Boolean m= a!=b;
+        System.out.println(m);
+
+
+
+
+
+    }
+}
