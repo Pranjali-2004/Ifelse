9dde59a1009640c785cf95a4565645d3fbe289c7
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-25 23:20:50 +0530

    Add files via upload

---
+++ inbuiltLibrary.java
@@ -0,0 +1,14 @@
+public class inbuiltLibrary {
+    public static void main(String[]args){
+        //Standard Inbuilt Library
+
+        //square root
+        System.out.println(Math.sqrt(5));
+
+      //  integer value by rounding down a float or double to the nearest integer
+        System.out.println(Math.floor(5.3));
+
+       // integer value by rounding up a float or double to the nearest integer
+        System.out.println(Math.ceil(5.3));
+    }
+}
