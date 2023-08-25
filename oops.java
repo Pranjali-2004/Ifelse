a664678a2dec649cd23a9e60f9ec1bc41f3650c0
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-25 23:12:06 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,13 @@
+public class oops {
+    //CREATING PROGRAM WITH OBJECT AND CLASS
+    int rollNo;
+    String studentName= "Pranjali";
+    public static void main(String[]args){
+        oops obj1= new oops();
+        System.out.println(obj1.rollNo);
+        System.out.println(obj1.studentName);
+
+
+
+    }
+}
