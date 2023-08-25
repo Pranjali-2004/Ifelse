78bb19d613260bea1cd04dfad9558bfd8c2c55d4
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-25 21:34:50 +0530

    Add files via upload

---
+++ scannerClass.java
@@ -0,0 +1,15 @@
+import java .util.Scanner;
+public class scannerClass {
+
+        public static void main(String[]args){
+            Scanner sc= new Scanner(System.in);
+            System.out.print("enter two no= ");
+            int n= sc.nextInt();
+            int m= sc.nextInt();
+            System.out.println("The no. is = "+n);
+            System.out.println("The no. is ="+m);
+
+
+        }
+    }
+
