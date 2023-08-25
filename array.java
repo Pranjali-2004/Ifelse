31883a36ccce57bfdcf555874ca746f13575113b
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-26 00:12:12 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[]args){
+        // PROGRAM TO DISPLAY
+        int[]age= new int[5];
+        age[0]= 14;
+        age[1]= 15;
+        age[2]= 20;
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+
+
+    }
+}
