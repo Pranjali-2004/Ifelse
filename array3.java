51fdcb82c7d845294399f44fe1e0e4f33a25e488
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-26 22:43:45 +0530

    Add files via upload

---
+++ array3.java
@@ -0,0 +1,15 @@
+public class array3 {
+    public static void main(String[]args){
+        //Program to run 2D array
+        int[][]age= new int[3][4];
+        age[0][1]= 34;
+        age[0][2]=21;
+        age[1][1]=32;
+        age[1][2]= 21;
+        System.out.println(age[0][1]);
+        System.out.println(age[0][2]);
+        System.out.println(age[1][1]);
+        System.out.println(age[1][2]);
+    }
+
+}
