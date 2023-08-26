3ce357f2a233ddffa3347a090061794281cc324a
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-08-26 22:43:20 +0530

    Add files via upload

---
+++ elementPresent.java
@@ -0,0 +1,11 @@
+public class elementPresent {
+    public static void main(String[]args){
+        int[]arr= {23,21,3,24,67};
+        int a= 21;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==a){
+                System.out.println("element is present in array");
+            }
+        }
+    }
+}
